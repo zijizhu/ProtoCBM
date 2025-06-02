ff05d30a2cb0a2f61047e87ad77d7db60444a9b3
Zhijie Zhu <zhuzhijie1003@outlook.com>
2025-06-03 00:44:38 +1000

    Fix

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #/!bin/bash
 
-bash train.sh resnet34 1
-bash train.sh densenet161 1
-bash train.sh resenet18 1
-bash train.sh densenet121 1
+bash scripts/train.sh resnet34 1
+bash scripts/train.sh densenet161 1
+bash scripts/train.sh resenet18 1
+bash scripts/train.sh densenet121 1
