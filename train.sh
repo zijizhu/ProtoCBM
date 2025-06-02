7ffc10ba3fb89860ebb5db2439da5a325e339aa5
Zhijie Zhu <zhuzhijie1003@outlook.com>
2025-06-03 00:43:58 +1000

    bug fix

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #/!bin/bash
 
-train.sh resnet34 1
-train.sh densenet161 1
-train.sh resenet18 1
-train.sh densenet121 1
+bash train.sh resnet34 1
+bash train.sh densenet161 1
+bash train.sh resenet18 1
+bash train.sh densenet121 1
